0c00cb20c022c78446ac15cf60b09064eb7fd7e5
Dongni An <da2899@columbia.edu>
2022-06-11 23:33:21 +0800

    Add Observer Pattern

---
+++ PatternObserver_WeatherDisplay/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PatternObserver_WeatherDisplay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
