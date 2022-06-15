8ca6e94255ae2bd3c55a729ee1cfbd319fced159
Dongni An <da2899@columbia.edu>
2022-06-15 21:38:33 +0800

    Add Singleton Pattern

---
+++ PatternSingleton_ChocolateBolier/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PatternSingleton_ChocolateBolier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
