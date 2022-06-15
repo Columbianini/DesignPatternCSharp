b966383e41010414ffa4484751ca34c611144e9b
Dongni An <da2899@columbia.edu>
2022-06-15 22:02:20 +0800

    Pattern Singleton: Method 1: private constructor＋static getInstance + static object Method 2: static object with initialization + static getInstance

---
+++ PatternSingleton_ChocolateBolier/ChocolateBoiler.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternSingleton_ChocolateBolier
+{
+    class ChocolateBoiler
+    {
+        public bool empty { get; private set; }
+        public bool boiled { get; private set; }
+
+        private static ChocolateBoiler chocolateBoiler;
+
+        private ChocolateBoiler()
+        {
+            empty = true;
+            boiled = false;
+        }
+
+        public static ChocolateBoiler getInstance()
+        {
+            if (chocolateBoiler == null)
+            {
+                chocolateBoiler = new ChocolateBoiler();
+            }
+            return chocolateBoiler;
+        }
+
+        public void fill()
+        {
+            if (empty)
+            {
+                empty = false;
+                boiled = false;
+            }
+        }
+
+        public void drain()
+        {
+            if(!empty && boiled)
+            {
+                empty = true;
+            }
+        }
+
+        public void boil()
+        {
+            if(!empty && !boiled)
+            {
+                boiled = true;
+            }
+        }
+    }
+
+
+
+    // Method 2: eagerly instantiation
+    class ChocolateBoiler2
+    {
+        public bool empty { get; private set; }
+        public bool boiled { get; private set; }
+
+        private static ChocolateBoiler2 chocolateBoiler = new ChocolateBoiler2();
+
+        private ChocolateBoiler2()
+        {
+            empty = true;
+            boiled = false;
+        }
+
+        public static ChocolateBoiler2 getInstance()
+        {
+            return chocolateBoiler;
+        }
+
+        public void fill()
+        {
+            if (empty)
+            {
+                empty = false;
+                boiled = false;
+            }
+        }
+
+        public void drain()
+        {
+            if (!empty && boiled)
+            {
+                empty = true;
+            }
+        }
+
+        public void boil()
+        {
+            if (!empty && !boiled)
+            {
+                boiled = true;
+            }
+        }
+    }
+
+}
--- PatternSingleton_ChocolateBolier/Program.cs
+++ PatternSingleton_ChocolateBolier/Program.cs
@@ -6,7 +6,18 @@ namespace PatternSingleton_ChocolateBolier
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ChocolateBoiler chocolate = ChocolateBoiler.getInstance();
+            chocolate.fill();
+            Console.WriteLine($"chocolateBoilder's status —— Empty: {chocolate.empty} boiled: {chocolate.boiled}");
+            ChocolateBoiler chocolate11 = ChocolateBoiler.getInstance();
+            Console.WriteLine($"Fetch the chocolateBoilder11's status again —— Empty: {chocolate.empty} boiled: {chocolate.boiled}");
+
+            ChocolateBoiler2 chocolate2 = ChocolateBoiler2.getInstance();
+            chocolate2.fill();
+            chocolate2.boil();
+            Console.WriteLine($"chocolateBoilder2's status —— Empty: {chocolate2.empty} boiled: {chocolate2.boiled}");
+            ChocolateBoiler2 chocolate22 = ChocolateBoiler2.getInstance();
+            Console.WriteLine($"Fetch the chocolateBoilder22's status again —— Empty: {chocolate2.empty} boiled: {chocolate2.boiled}");
         }
     }
 }
