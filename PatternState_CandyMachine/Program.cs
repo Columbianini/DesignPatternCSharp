e493cd9ad23d252bb97035b294f978c7920fcda9
Dongni An <da2899@columbia.edu>
2022-06-25 17:54:14 +0800

    PatternState: Our Candy Machine has 4 state: NO_QUARTER/HAS_QUARTER/SOLD/SOLD_OUT and 5 methods for state transferring: insertQuarter/ejectQuarter/turnCrank/dispense

---
+++ PatternState_CandyMachine/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PatternState_CandyMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GumballMachine gumballMachine = new GumballMachine(5);
+
+            Console.WriteLine(gumballMachine);
+
+            gumballMachine.insertQuarter();
+            gumballMachine.turnCrank();
+
+            Console.WriteLine(gumballMachine);
+
+            gumballMachine.insertQuarter();
+            gumballMachine.ejectQuarter();
+            gumballMachine.turnCrank();
+
+            Console.WriteLine(gumballMachine);
+
+            gumballMachine.insertQuarter();
+            gumballMachine.turnCrank();
+            gumballMachine.insertQuarter();
+            gumballMachine.turnCrank();
+            gumballMachine.ejectQuarter();
+
+            Console.WriteLine(gumballMachine);
+
+            gumballMachine.insertQuarter();
+            gumballMachine.insertQuarter();
+            gumballMachine.turnCrank();
+            gumballMachine.insertQuarter();
+            gumballMachine.turnCrank();
+            gumballMachine.insertQuarter(); // SOLD OUT ALREADY
+            gumballMachine.turnCrank();
+
+            Console.WriteLine(gumballMachine);
+        }
+    }
+}
